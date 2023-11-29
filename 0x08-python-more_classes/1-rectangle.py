489fa8aa1ab6450aedb9fa8979962c8a0e719328
you-desing <youssefboulahya3@gmail.com>
2023-11-29 04:14:36 -0800

    python3

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""rectangle file"""
+class Rectangle:
+    """ class defines a rectangle """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+    
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Width must be an integer")
+        if value < 0:
+            raise ValueError("Width must be >= 0")
+        self.__width = value
+    
+    @property
+    def height(self):
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Height must be an integer")
+        if value < 0:
+            raise ValueError("Height must be >= 0")
+        self.__height = value
