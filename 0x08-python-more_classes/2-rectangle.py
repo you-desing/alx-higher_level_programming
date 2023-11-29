97de8b212771602c8a089938bb6735cb2162aa87
you-desing <youssefboulahya3@gmail.com>
2023-11-29 04:36:05 -0800

    python

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Rectangle module"""
+
+class Rectangle:
+    """Class defines a rectangle."""
+    def __init__(self, width=0, height=0):
+        """Initialize the rectangle with given width and height."""
+        self.__width = width
+        self.__height = height
+    
+    @property
+    def width(self):
+        """Get the width of the rectangle."""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("Width must be an integer")
+        if value < 0:
+            raise ValueError("Width must be >= 0")
+        self.__width = value
+    
+    @property
+    def height(self):
+        """Get the height of the rectangle."""
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("Height must be an integer")
+        if value < 0:
+            raise ValueError("Height must be >= 0")
+        self.__height = value
+    def area(self):
+        """Area of the rectangle."""
+        Area = self.width * self.height
+        return Area
+    def perimeter(self):
+        """Perimeter of the rectangle."""
+        Parmt = self.width + self.height
+        return Parmt
