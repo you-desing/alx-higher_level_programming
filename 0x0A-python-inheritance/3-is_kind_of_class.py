75cf7333d3d36ae14a9beb7b1d76f7c77f952fa3
you-desing <youssefboulahya3@gmail.com>
2023-12-04 07:27:53 -0800

    code python

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" kind of class """
+
+
+def is_kind_of_class(obj, a_class):
+    """class is same"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
