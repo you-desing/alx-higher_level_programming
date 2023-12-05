f54ac484c9bad9ce6b51e8d0b87fd7118fdf65e9
you-desing <youssefboulahya3@gmail.com>
2023-12-05 04:37:20 -0800

    python

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
-"""BaseGeometry class definition"""
+"""Rectangle file"""
 
 
 class BaseGeometry:
     """base class"""
     def area(self):
-        """method area"""
-        raise Exception("area() is not implement")
+        """area method"""
+        raise Exception("area() is not implemented")
     def integer_validator(self, name, value):
-        """ Validates that the given value is an integer greater than 0"""
+        """integer validate"""
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 class Rectangle(BaseGeometry):
-    """ reactangle class"""
+    """rectangle class"""
     def __init__(self, width, height):
-        """Validate width and height using the integer_validator from the base class"""
         super().integer_validator("width", width)
         super().integer_validator("height", height)
         self.__width = width
         self.__height = height
     def area(self):
-        """area rectangle"""
         return self.__width * self.__height
+
     def __str__(self):
-        """ str method"""
-        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+        return "[Rectangle] " + str (self.__width) + "/" + str(self.__height)
 class Square(Rectangle):
     """square class"""
     def __init__(self, size):
