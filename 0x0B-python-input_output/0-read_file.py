3f83b58342964ed29fc582be309b402746891f87
you-desing <youssefboulahya3@gmail.com>
2023-12-06 11:35:09 -0800

    python

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """function read"""
-    with open(filename,"r") as file:
+    with open(filename, "r", encoding='utf-8') as file:
         file_cont = file.read()
     print(file_cont)
