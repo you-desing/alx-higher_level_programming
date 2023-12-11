ac35fa1492989249b9f0ff6f0f9f34528e14ca80
you-desing <youssefboulahya3@gmail.com>
2023-12-11 08:32:31 -0800

    cercle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,15 +1,32 @@
 #!/usr/bin/python3
-"""task 1"""
+'''Module for Base class.'''
+from json import dumps, loads
 
 
 class Base:
-    """class base"""
+    '''A representation of the base of our OOP hierarchy.'''
+
     __nb_objects = 0
+
     def __init__(self, id=None):
-        self.id = id 
+        '''Constructor.'''
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-        return self.id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''Jsonifies a dictionary so it's quite rightly and longer.'''
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''Unjsonifies a dictionary.'''
+        if json_string is None or not json_string:
+            return []
+        return loads(json_string)
