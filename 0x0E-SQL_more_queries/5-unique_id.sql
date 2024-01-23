1327aa7c402644c21a781cc7e54620bd8dc2e7ca
you-desing <youssefboulahya3@gmail.com>
2024-01-23 05:30:29 -0800

    sql

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,5 @@
 -- Create the id_not_null table if it does not exist
-CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT DEFAULT 1,
-    name VARCHAR(256),
-    PRIMARY KEY(id)
+CREATE TABLE IF NOT EXISTS unique_id(
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
 );
