d6e7502ad52348aa2c98b882d102392a4f20e5b3
you-desing <youssefboulahya3@gmail.com>
2024-01-23 04:31:23 -0800

    sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,10 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Create MySQL user user_0d_2 if not exists
-CREATE USER IF NOT EXISTS 'user_0d_2'@'%';
-
--- Set password for user_0d_2
-ALTER USER 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant SELECT privilege to user_0d_2 on hbtn_0d_2 database
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
